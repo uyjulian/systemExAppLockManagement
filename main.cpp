08a1069c5ebb9fdeb6492c8cb89b05323b7bf04f
uyjulian <uyjulian@gmail.com>
2021-02-16 13:09:26 -0600

    Actually commit main source file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 
 struct System
 {
-	static void setArgument(ttstr name, ttstr value)
+	static tTVInteger createAppLock(ttstr lockname)
 	{
-		TVPSetCommandLine(name.c_str(), value);
+		// lock application using mutex
+		HANDLE mutex = CreateMutex(NULL, TRUE, lockname.c_str());
+
+		if (GetLastError() == ERROR_ALREADY_EXISTS && mutex != NULL)
+		{
+			ReleaseMutex(mutex);
+			return 0; // already running
+		}
+		return (tTVInteger)mutex;
+	}
+
+	static bool releaseAppLock(tTVInteger lock)
+	{
+		if (lock == 0)
+		{
+			return false;
+		}
+		return !!ReleaseMutex((HANDLE)lock);
 	}
 };
 
-NCB_ATTACH_FUNCTION(setArgument, System, System::setArgument);
+NCB_ATTACH_FUNCTION(createAppLock, System, System::createAppLock);
+NCB_ATTACH_FUNCTION(releaseAppLock, System, System::releaseAppLock);
